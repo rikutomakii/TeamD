c305176937edb6d5af04035500074ad2d5773953
muramaruyuuki <kbc18b27@stu.kawahara.ac.jp>
2020-11-13 02:34:35 +0900

    追加

---
+++ making_is_GOD/Assets/C#/EnemyGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2f, 0.5f);
+    }
+    void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-15.0f, 15.0f),
+            transform.position.y,
+            transform.position.z
+            );
+        Instantiate(
+         enemyPrefab,
+         spawnPosition,
+         transform.rotation
+         );
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ making_is_GOD/Assets/C#/EnemyZako.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyZako : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position -= new Vector3(0,10*Time.deltaTime,0);
+    }
+}
